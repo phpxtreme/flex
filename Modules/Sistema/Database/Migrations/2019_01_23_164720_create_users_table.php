45919780d43eeff594a09d40e438ab1a98b4c730
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2019-01-23 11:48:49 -0500

    First Commit

---
+++ Modules/Sistema/Database/Migrations/2019_01_23_164720_create_users_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Users Table
+     *
+     * @var string
+     */
+    private $table = 'users';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('first_name')
+                ->nullable(false);
+
+            $table->string('last_name')
+                ->nullable(false);
+
+            $table->string('email')
+                ->unique();
+
+            $table->text('password')
+                ->nullable(false);
+
+            $table->boolean('active')
+                ->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
