5ad7da8585810382792953ae309b9235028122cb
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2019-01-23 17:55:06 -0500

    First Commit

--- app/Console/Commands/MakeRepository.php
+++ app/Console/Commands/MakeRepository.php
@@ -3,9 +3,17 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Nwidart\Modules\Support\Config\GenerateConfigReader;
 
 class MakeRepository extends GeneratorCommand
 {
+    /**
+     * Store input information
+     *
+     * @var array
+     */
+    protected $data = [];
+
     /**
      * The type of class being generated.
      *
@@ -35,7 +43,23 @@ class MakeRepository extends GeneratorCommand
     public function handle()
     {
         $module = $this->choice('In which module will the repository be generated?', $this->getModules());
-        $entity = $this->ask('For what entity will the repository be generated?');
+        $entity = $this->choice('For what entity will the repository be generated?', $this->getEntities());
+
+        $this->data = [
+            'module' => $module,
+            'entity' => $entity
+        ];
+
+        if ($this->alreadyExists($this->getNameInput())) {
+            $this->error($this->type . ' already exists!');
+            return false;
+        }
+
+        /** @var string $name */
+        $name = $this->qualifyClass($this->getNameInput());
+
+        $this->files->put($this->getPath($name), $this->buildClass($name));
+        $this->info($this->type . ' created successfully!');
     }
 
     /**
@@ -67,9 +91,49 @@ class MakeRepository extends GeneratorCommand
 
     protected function getEntities()
     {
+        //TODO: This!
+
         /** @var array $entities */
-        $entities = [];
+        $entities = [1, 2, 3];
 
         return $entities;
+    }
+
+    /**
+     * Get the desired class name from the input.
+     *
+     * @return string
+     */
+    protected function getNameInput()
+    {
+        return trim($this->data['entity']) . 'Repository';
+    }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string $stub
+     * @param  string $name
+     *
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        return parent::replaceClass($stub, $name);
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string $name
+     *
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $repository = GenerateConfigReader::read('repository');
+        $modulePath = $this->laravel['modules']->getModulePath($this->data['module']);
+
+        return $modulePath . $repository->getPath() . '/' . studly_case($this->data['entity']) . 'Repository.php';
     }
 }
