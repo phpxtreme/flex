d97eb35ac56c2cdad2a3adb65ee37b2ba97cecb8
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2019-01-25 15:01:15 -0500

    First Commit

---
+++ Modules/Sistema/Config/config.php
@@ -0,0 +1,40 @@
+<?php
+return [
+
+    'name' => 'Sistema',
+
+    'init' => [
+
+        // Roles
+        'roles' => [
+            [
+                'name'   => 'Admin',
+                'active' => true
+            ],
+            [
+                'name'   => 'User',
+                'active' => true
+            ]
+        ],
+
+        // Users
+        'users' => [
+            [
+                'email'      => 'admin@example.com',
+                'password'   => 'ascent',
+                'first_name' => 'Administrator',
+                'last_name'  => 'Administrator',
+                'role_id'    => 1,
+                'active'     => true
+            ],
+            [
+                'email'      => 'user@example.com',
+                'password'   => 'ascent',
+                'first_name' => 'User',
+                'last_name'  => 'User',
+                'role_id'    => 2,
+                'active'     => true
+            ]
+        ]
+    ]
+];
