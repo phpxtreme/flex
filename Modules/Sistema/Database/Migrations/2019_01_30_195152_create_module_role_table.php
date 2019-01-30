c0437489d6ae0ab30c39555812e0afff31c23754
Leonardo Yanes Batista <leo.phpxtreme@gmail.com>
2019-01-30 16:21:02 -0500

    First Commit

---
+++ Modules/Sistema/Database/Migrations/2019_01_30_195152_create_module_role_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModuleRoleTable extends Migration
+{
+    private $table = 'pivot_module_role';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('role_id')
+                ->nullable(false);
+
+            $table->integer('module_id')
+                ->nullable(false);
+
+            $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')->on('roles')
+                ->onDelete('cascade');
+
+            $table->foreign('module_id')
+                ->references('id')->on('modules')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
